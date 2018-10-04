7c987e2158acd5c07b3f27b6188ffca659d47290
Rajesh Chikkala <rajeshd.chikkala@gmail.com>
2018-10-03 21:57:59 -0400

    comiiting changes

---
+++ HelloWorld/src/com/rajesh/java/basic/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.rajesh.java.basic;
+
+/**
+ * @author Rajesh Chikkala
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Worl");
+	}
+
+}
